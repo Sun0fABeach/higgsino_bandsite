a8b383939944af7274594593f8f8e3fc111f73ef
Markus Appel <markospoparkos@gmail.com>
2017-07-19 18:09:29 +0200

    Port scripting to jquery

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 (function() {
 'use strict';
 
-window.addEventListener('load', function() {
+$(window).on('load', function() {
+
     // initial fade-in of logo and backdrop
-    var logo = document.querySelector('#logo');
-    do_initial_fade_in(logo, document.querySelector('#background-pic'));
+    display_logo();
+    $('#background-pic').css('opacity', '1');
+
 
     // launch animations of the backdrop
-    var background = document.querySelector('#background');
     function next_animation() {
-        background.style.animation = rotate_animation();
+        $('#background').css('animation', rotate_animation());
     }
     next_animation();
-    background.addEventListener('animationend', next_animation);
+    $('#background').on('animationend', next_animation);
 
-    // mute / unmute background track
-    var mute_toggle = document.querySelector('#mute-toggle');
-    mute_toggle.addEventListener('click', function() {
-        toggle_mute(mute_toggle.firstElementChild);
-    });
 
     // click on logo: open content box, hide logo and deactivate animations
-    var content_box = document.querySelector('main');
-    var content_wrapper = content_box.firstElementChild;
     function open_content_box() {
-        display_content_box(content_box, content_wrapper);
-        hide_logo(logo);
-        logo.removeEventListener('click', open_content_box);
-        background.style.animation = null;
+        display_content_box();
+        hide_logo();
+        $('#background').css('animation', '');
     }
-    logo.addEventListener('click', open_content_box);
+    $('#logo').one('click', open_content_box);
 
     // add scrollbars to content box only after opening transition completed
-    content_box.addEventListener('transitionend', function(event) {
+    $('main').on('transitionend', function(event) {
         if(event.propertyName === 'opacity' &&
-            content_box.style.visibility === 'visible')
-                content_box.style.overflow = 'auto';
+            $(this).css('visibility') === 'visible')
+                $(this).css('overflow', 'auto');
     });
 
+
     // on close: hide content box, show logo, reactivate animations
-    var close_target = document.querySelector('#close-target');
-    close_target.addEventListener('click', function() {
-        hide_content_box(content_box, content_wrapper);
-        display_logo(logo);
-        logo.addEventListener('click', open_content_box);
+    $('#close-target').click(function() {
+        hide_content_box();
+        display_logo();
+        $('#logo').one('click', open_content_box);
         next_animation();
-        background.addEventListener('animationend', next_animation);
+        $('#background').on('animationend', next_animation);
+    });
+
+
+    // mute / unmute background track
+    var mute_toggle_icons = {
+        mute: 'fa fa-volume-up',
+        unmute: 'fa fa-volume-off'
+    };
+    $('#mute-toggle').click(function() {
+        var mute_icon = $(this).children('i');
+        var player = $('audio').get(0);
+        if(player.muted) {
+            player.muted = false;
+            mute_icon.removeClass().addClass(mute_toggle_icons.mute);
+        } else {
+            player.muted = true;
+            mute_icon.removeClass().addClass(mute_toggle_icons.unmute);
+        }
     });
 });
 
-
-function do_initial_fade_in(logo, background_pic) {
-    logo.style.opacity = 1;
-    background_pic.style.opacity = 1;
-}
 
 var animations = [
     'zoom 700ms ease-in 9s',
@@ -66,45 +72,40 @@ function rotate_animation() {
     return anim;
 }
 
-var mute_toggle_icons = {
-    mute: 'fa fa-volume-up',
-    unmute: 'fa fa-volume-off'
-};
-function toggle_mute(mute_icon) {
-    var player = document.querySelector('audio');
-    if(player.muted) {
-        player.muted = false;
-        mute_icon.className = mute_toggle_icons.mute;
-    } else {
-        player.muted = true;
-        mute_icon.className = mute_toggle_icons.unmute;
-    }
+function display_content_box() {
+    $('main').css({
+        visibility: 'visible',
+        maxWidth: '80%',
+        maxHeight: '90%'
+    }).children('#content-wrapper').css(
+        'opacity', '1'
+    );
 }
 
-function display_content_box(content_box, content_wrapper) {
-    content_box.style.visibility = 'visible';
-    content_box.style.maxWidth = '80%';
-    content_box.style.maxHeight = '90%';
-    content_wrapper.style.opacity = 1;
+function hide_content_box() {
+    $('main').css({
+        overflow: 'hidden',
+        visibility: 'hidden',
+        maxWidth: '0',
+        maxHeight: '0'
+    }).children('#content-wrapper').css(
+        'opacity', '0'
+    );
 }
 
-function hide_content_box(content_box, content_wrapper) {
-    content_box.style.overflow = 'hidden';
-    content_box.style.visibility = 'hidden';
-    content_box.style.maxWidth = 0;
-    content_box.style.maxHeight = 0;
-    content_wrapper.style.opacity = 0;
+function display_logo() {
+    $('#logo').css({
+        opacity: '1',
+        cursor: 'pointer'
+    });
 }
 
-function display_logo(logo) {
-    logo.style.opacity = 1;
-    logo.style.cursor = 'pointer';
-}
-
-function hide_logo(logo) {
-    logo.style.transition = 'opacity 0.5s linear';
-    logo.style.opacity = 0;
-    logo.style.cursor = 'auto';
+function hide_logo() {
+    $('#logo').css({
+        transition: 'opacity 0.5s linear',
+        opacity: '0',
+        cursor: 'auto'
+    });
 }
 
 })();
