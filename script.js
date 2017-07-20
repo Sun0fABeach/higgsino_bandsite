4885f38a3c48cc270471cee1f08d07a78bd40742
Markus Appel <markospoparkos@gmail.com>
2017-07-20 17:13:48 +0200

    Minor beautifications

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ $(window).on('load', function() {
 
 
     // click on logo: open content box, hide logo
-    function open_content_box() {
+    function open_content() {
         display_content_box();
         hide_logo();
     }
-    $('#logo').one('click', open_content_box);
+    $('#logo').one('click', open_content);
 
     // add scrollbars to content box only after opening transition completed
     $('main').on('transitionend', function(event) {
@@ -27,7 +27,7 @@ $(window).on('load', function() {
     $('#close-target').click(function() {
         hide_content_box();
         display_logo();
-        $('#logo').one('click', open_content_box);
+        $('#logo').one('click', open_content);
     });
 
 
@@ -37,19 +37,21 @@ $(window).on('load', function() {
         unmute: 'fa fa-volume-off'
     };
     $('#mute-toggle').click(function() {
-        var mute_icon = $(this).children('i');
+        var $mute_icon = $(this).children('i');
         var player = $('audio').get(0);
         if(player.muted) {
             player.muted = false;
-            mute_icon.removeClass().addClass(mute_toggle_icons.mute);
+            $mute_icon.removeClass().addClass(mute_toggle_icons.mute);
         } else {
             player.muted = true;
-            mute_icon.removeClass().addClass(mute_toggle_icons.unmute);
+            $mute_icon.removeClass().addClass(mute_toggle_icons.unmute);
         }
     });
 });
 
-
+/** helpers for animation - we are not using jQuery's animation handlers
+ *  because they lack customization options (easing, delay)
+ */
 function display_content_box() {
     $('main').css({
         visibility: 'visible',
