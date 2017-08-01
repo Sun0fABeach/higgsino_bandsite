96b4421e41c445ae8dee25d76f69d6791c9a7673
Markus Appel <markospoparkos@gmail.com>
2017-08-01 13:55:18 +0200

    Cleanup script code

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 (function() {
 'use strict';
 
+/* font-awesome icon classes */
+var mute_toggle_icons = {
+    mute: 'fa fa-volume-up',
+    unmute: 'fa fa-volume-off'
+};
+
+
 $(document).ready(function() {
     hide_content_box();
-});
-
-$(window).on('load', function() {
-
-    // initial fade-in of logo and backdrop + subsequent zoom animation
-    display_logo();
-    $('#background-pic').css('opacity', '1');
-    $('#background').css('animation', 'zoom 700ms ease-in 9s');
-
 
     // click on logo: open content box, hide logo
-    function open_content() {
-        display_content_box();
-        hide_logo();
-    }
     $('#logo').one('click', open_content);
-
-    // add scrollbars to content box only after opening transition completed
-    $('main').on('transitionend', function(event) {
-        if(event.propertyName === 'opacity' &&
-            $(this).css('visibility') === 'visible')
-                $(this).css('overflow', 'auto');
-    });
-
 
     // on close: hide content box, show logo
     $('#close-target').click(function() {
@@ -35,12 +21,7 @@ $(window).on('load', function() {
         $('#logo').one('click', open_content);
     });
 
-
     // mute / unmute background track
-    var mute_toggle_icons = {
-        mute: 'fa fa-volume-up',
-        unmute: 'fa fa-volume-off'
-    };
     $('#mute-toggle').click(function() {
         var $mute_icon = $(this).children('i');
         var player = $('audio').get(0);
@@ -52,6 +33,16 @@ $(window).on('load', function() {
             $mute_icon.removeClass().addClass(mute_toggle_icons.unmute);
         }
     });
+});
+
+
+/** launch initial fade-in of logo and backdrop + subsequent zoom animation
+ *  once all assets have been loaded.
+ */
+$(window).on('load', function() {
+    display_logo();
+    $('#background-pic').css('opacity', '1');
+    $('#background').css('animation', 'zoom 700ms ease-in 9s');
 
 });
 
@@ -59,13 +50,18 @@ $(window).on('load', function() {
 /** helpers for animation - we are not using jQuery's animation handlers
  *  because they lack customization options (easing, delay)
  */
+ function open_content() {
+     display_content_box();
+     hide_logo();
+ }
+
 function display_content_box() {
     $('#background').css('animation', '');
     $('main').css('left', 0);
 }
 
 function hide_content_box() {
-    $('main').css('left', '-' + $('main').outerWidth() + 'px')
+    $('main').css('left', '-' + $('main').outerWidth() + 'px');
 }
 
 function display_logo() {
